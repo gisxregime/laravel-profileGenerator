c6d80ef23f16760d8f8cb21333fedd276d557302
gisxregime <195636284+gisxregime@users.noreply.github.com>
2026-04-16 09:48:48 +0800

    initial

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 
-Route::get('/', [ProfileController::class, 'index']);
 Route::get('/profile', [ProfileController::class, 'index']);
 Route::post('/profile', [ProfileController::class, 'store']);
 Route::delete('/profile/clear', [ProfileController::class, 'clearAll']);
